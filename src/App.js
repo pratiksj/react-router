4fb929ed9b6989a1c2f08fd6b3590e105281e26f
pratiksj <pratiksharai728@gmail.com>
2022-12-22 17:15:36 +0545

    feat:parameterized the route by using useNavigate and useMatch hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import {
+  Routes,
+  Route,
+  Link,
+  useParams,
+  useNavigate,
+  Navigate,
+  useMatch,
+} from "react-router-dom";
+import { useState } from "react";
 
 const Home = () => (
   <div>
@@ -7,12 +16,50 @@ const Home = () => (
   </div>
 );
 
-const Notes = () => (
+const Notes = ({ notes }) => (
   <div>
-    {" "}
-    <h2>react</h2>{" "}
+    <h2>Notes</h2>
+    <ul>
+      {notes.map((note) => (
+        <li key={note.id}>
+          <Link to={`/notes/${note.id}`}>{note.content}</Link>
+        </li>
+      ))}
+    </ul>
   </div>
 );
+
+// const Notes = () => (
+//   <div>
+//     {" "}
+//     <h2>react</h2>{" "}
+//   </div>
+// );
+
+const Login = (props) => {
+  const navigate = useNavigate();
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    props.onLogin("mluukkai");
+    navigate("/");
+  };
+
+  return (
+    <div>
+      <h2>login</h2>
+      <form onSubmit={onSubmit}>
+        <div>
+          username: <input />
+        </div>
+        <div>
+          password: <input type="password" />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
+  );
+};
 
 const Users = () => (
   <div>
@@ -20,13 +67,57 @@ const Users = () => (
     <h2>Pratiksha</h2>{" "}
   </div>
 );
+const Note = ({ note }) => {
+  // const id = useParams().id;
+  // const note = notes.find((n) => n.id === Number(id));
+  return (
+    <div>
+      <h2>{note.content}</h2>
+      <div>{note.user}</div>
+      <div>
+        <strong>{note.important ? "important" : ""}</strong>
+      </div>
+    </div>
+  );
+};
 
 function App() {
+  const match = useMatch("/notes/:id");
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      content: "HTML is easy",
+      important: true,
+      user: "Matti Luukkainen",
+    },
+    {
+      id: 2,
+      content: "Browser can execute only JavaScript",
+      important: false,
+      user: "Matti Luukkainen",
+    },
+    {
+      id: 3,
+      content: "Most important methods of HTTP-protocol are GET and POST",
+      important: true,
+      user: "Arto Hellas",
+    },
+  ]);
+
+  const note = match
+    ? notes.find((note) => note.id === Number(match.params.id))
+    : null;
+
+  const [user, setUser] = useState(null);
+  const login = (user) => {
+    setUser(user);
+  };
+
   const padding = {
     padding: 5,
   };
   return (
-    <Router>
+    <div>
       <div>
         <Link style={padding} to="/">
           home
@@ -37,13 +128,26 @@ function App() {
         <Link style={padding} to="/users">
           users
         </Link>
+        {user ? (
+          <em>{user} logged in</em>
+        ) : (
+          <Link style={padding} to="/login">
+            login
+          </Link>
+        )}
       </div>
       <Routes>
-        <Route path="/notes" element={<Notes />} />
-        <Route path="/users" element={<Users />} />
+        <Route path="/notes/:id" element={<Note note={note} />} />
+        <Route path="/notes" element={<Notes notes={notes} />} />
+        {/* <Route path="/users" element={<Users />} /> */}
+        <Route
+          path="/users"
+          element={user ? <Users /> : <Navigate replace to="/login" />}
+        />
+        <Route path="/login" element={<Login onLogin={login} />} />
         <Route path="/" element={<Home />} />
       </Routes>
-    </Router>
+    </div>
   );
 }
 
