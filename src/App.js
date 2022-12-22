bb8068891954430876f21edfb12afd587012cf00
pratiksj <pratiksharai728@gmail.com>
2022-12-22 14:11:32 +0545

    feat: initial navigation bar containing multiple view using Link and Route

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+const Home = () => (
+  <div>
+    {" "}
+    <h2>javascript</h2>{" "}
+  </div>
+);
+
+const Notes = () => (
+  <div>
+    {" "}
+    <h2>react</h2>{" "}
+  </div>
+);
+
+const Users = () => (
+  <div>
+    {" "}
+    <h2>Pratiksha</h2>{" "}
+  </div>
+);
 
 function App() {
+  const padding = {
+    padding: 5,
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Link style={padding} to="/">
+          home
+        </Link>
+        <Link style={padding} to="/notes">
+          notes
+        </Link>
+        <Link style={padding} to="/users">
+          users
+        </Link>
+      </div>
+      <Routes>
+        <Route path="/notes" element={<Notes />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
